2a978f539a5e0495cb5ce7d567afc1a1ecfacfe6
Zarkhah.Ashkan <ashkanzarkhah@gmail.com>
2021-03-02 19:50:00 +0330

    charidaning :)

--- App1/urls.py
+++ App1/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from . import views
 
+app_name = "App1"
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="write"),
+    path('<slug>', views.app_detail, name="detail"),
 ]
--- App1/views.py
+++ App1/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from . import models
 
 # Create your views here.
 def home(request):
     test = models.Apps1.objects.all().order_by('date')
     return render(request, 'App1/home.html',{'test': test})
+
+def app_detail(request, slug):
+    return HttpResponse(slug)
