3540127a0865a79b787a73986f33dbec0130ad9c
Zarkhah.Ashkan <ashkanzarkhah@gmail.com>
2021-03-07 12:20:56 +0330

    complete master login !

--- masteraccounts/views.py
+++ masteraccounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def sign_up(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('masteraccounts:main')
     else: form = UserCreationForm()
     return render(request, 'masteraccounts/signup.html', {'form' : form})
@@ -16,6 +18,8 @@ def log_in(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
+            user = form.get_user()
+            login(request, user)
             return redirect('masteraccounts:main')
 
     else:
